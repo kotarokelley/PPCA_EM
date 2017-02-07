e4ef4239605624a8346558747337bd9bb0662a3f
kotarokelley <kotarokelley@dhcp-18-111-4-198.dyn.mit.edu>
2017-02-07 10:49:08 -0500

    Changed Parser to be able to write data. Got rid of some unnecessary variables in class PPCA.

---
+++ PPCA_EM/include/PPCA_mpi.h
@@ -0,0 +1,15 @@
+/*
+ * PPCA_mpi.h
+ *
+ *  Created on: Feb 6, 2017
+ *      Author: kotarokelley
+ */
+
+#ifndef PPCA_MPI_H_
+#define PPCA_MPI_H_
+
+
+
+
+
+#endif /* PPCA_MPI_H_ */
---
+++ PPCA_EM/src/PPCA_mpi.cpp
@@ -0,0 +1,10 @@
+/*
+ * PPCA_mpi.cpp
+ *
+ *  Created on: Feb 6, 2017
+ *      Author: kotarokelley
+ */
+
+
+
+
