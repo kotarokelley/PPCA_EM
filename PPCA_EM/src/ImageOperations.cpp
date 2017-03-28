79045292f3f3aa3bd61d3fedcc2e6489628a75d7
kotarokelley <kotarokelley@dhcp-18-111-28-38.dyn.mit.edu>
2017-03-28 16:41:08 -0400

    Functions transform_Img_single and _multiple are up and running.

--- PPCA_EM/src/ImageOperations.cpp
+++ PPCA_EM/src/ImageOperations.cpp
@@ -50,7 +50,7 @@ void transform_Img_single(const double* inImg, double* outImg, int xpix, int ypi
 
 				int x1 = std::floor(x_new), x2 = std::ceil(x_new), y1 = std::ceil(y_new), y2 = std::floor(y_new);
 
-				if (x1== x2)						// This takes care of landing exactly on a grid point.
+				if (x1 == x2)						// This takes care of landing exactly on a grid point.
 					x2 +=1;
 				if (y2 == y1)
 					y1 +=1;
@@ -62,7 +62,7 @@ void transform_Img_single(const double* inImg, double* outImg, int xpix, int ypi
 				yy1 = y_new - y1;
 				xx1 = x_new - x1;
 
-				outImg[j*xpix + i] = //inImg[y1*xpix + x1];
+				outImg[j*xpix + i] =
 					1.0 / (x2x1 * y2y1) *
 					(inImg[y1*xpix + x1] * x2x * y2y +
 					inImg[y1*xpix + x2] * xx1 * y2y +
@@ -78,24 +78,29 @@ void transform_Img_multiple(const double* inImg, double* outImg, int xpix, int y
 
 	mat trans_mat(3,3,fill::zeros);
 	trans_mat(0,0) = std::cos(theta);
-	trans_mat(0,1) = -std::sin(theta);
+	trans_mat(0,1) = std::sin(theta);
 	trans_mat(0,2) = delta_x;
-	trans_mat(1,0) = std::sin(theta);
+	trans_mat(1,0) = -std::sin(theta);
 	trans_mat(1,1) = std::cos(theta);
 	trans_mat(1,2) = delta_y;
 	trans_mat(2,2) = 1.0;
 
-
 	double* mean = new double[numImg]();					// Pre-compute the mean of each image to be used for out of bounds conditions.
+
 	for (int k=0; k<numImg; k++){
+
 		int offset = k*xpix*ypix;
+
 		for (int m=0; m<xpix*ypix; m++)
 			mean[k] += inImg[m+offset];
+
 		mean[k] /= xpix*ypix;
 	}
 
 	for (int i=0; i<xpix; i++){
+
 		for (int j=0; j<ypix; j++){
+
 			vec coords_old(3);
 			coords_old(0) = i-xpix/2;					// Set center of image at xpix/2 and ypix/2
 			coords_old(1) = j-ypix/2;
@@ -104,7 +109,7 @@ void transform_Img_multiple(const double* inImg, double* outImg, int xpix, int y
 			vec coords_new = trans_mat * coords_old;	// Apply transformation matrix.
 
 			double x_new = coords_new(0) + xpix/2;		// Re set center image at origin (0,0).
-			double y_new = coords_new(0) + ypix/2;
+			double y_new = coords_new(1) + ypix/2;
 
 			if (x_new>=xpix || y_new>=ypix || std::sqrt(std::pow(x_new/2,2) + std::pow(y_new/2,2)) > max_radius){
 				for (int k=1; k < numImg; k++)
@@ -112,22 +117,39 @@ void transform_Img_multiple(const double* inImg, double* outImg, int xpix, int y
 			}
 
 			else{
+
 				double x2x1, y2y1, x2x, y2y, yy1, xx1;
-				int x1 = std::floor(x_new), x2 = std::ceil(x_new), y1 = std::floor(y_new), y2 = std::ceil(y_new);
+
+				int x1 = std::floor(x_new), x2 = std::ceil(x_new), y1 = std::ceil(y_new), y2 = std::floor(y_new);
+
+				if (x1 == x2)						// This takes care of landing exactly on a grid point.
+					x2 +=1;
+				if (y2 == y1)
+					y1 +=1;
+
 				x2x1 = x2 - x1;
 				y2y1 = y2 - y1;
 				x2x = x2 - x_new;
 				y2y = y2 - y_new;
 				yy1 = y_new - y1;
 				xx1 = x_new - x1;
+
 				for (int k=1; k < numImg; k++){
 					int offset = k*xpix*ypix;
-					outImg[i*xpix + j + offset] =
+
+					outImg[j*xpix + i + offset] =
 						1.0 / (x2x1 * y2y1) *
-						(inImg[x1*xpix + y1 + offset] * x2x * y2y +
-						inImg[x2*xpix + y1 + offset] * xx1 * y2y +
-						inImg[x1*xpix + y2 + offset] * x2x * yy1 +
-						inImg[x2*xpix + y2 + offset] * xx1 * yy1);
+						(inImg[y1*xpix + x1 + offset] * x2x * y2y +
+						inImg[y1*xpix + x2 + offset] * xx1 * y2y +
+						inImg[y2*xpix + x1 + offset] * x2x * yy1 +
+						inImg[y2*xpix + x2 + offset] * xx1 * yy1);
+
+					//outImg[i*xpix + j + offset] =
+						//1.0 / (x2x1 * y2y1) *
+						//(inImg[x1*xpix + y1 + offset] * x2x * y2y +
+						//inImg[x2*xpix + y1 + offset] * xx1 * y2y +
+						//inImg[x1*xpix + y2 + offset] * x2x * yy1 +
+						//inImg[x2*xpix + y2 + offset] * xx1 * yy1);
 				}
 			}
 		}
--- PPCA_EM/test/test_ImagOperations.cpp
+++ PPCA_EM/test/test_ImagOperations.cpp
@@ -47,6 +47,7 @@ int main(void){
 		f_data_double[i] = f_data_float[i];
 	}
 
+	std::cout << "Testing function transform_Img_single\n";
 	std::cout << "Displaying first image\n";
 	cv::Mat img_0(cv::Size(f_dim[0],f_dim[1]),CV_64F,f_data_double);
 	cv::namedWindow("image_0",cv::WINDOW_NORMAL);
@@ -54,12 +55,45 @@ int main(void){
 	cv::waitKey(2000);
 
 	double * rotated_img_0 = new double[f_dim[0]*f_dim[1]]();
-	transform_Img_single(f_data_double, rotated_img_0, f_dim[0], f_dim[1], 0,  0,  360,  f_dim[0]/2 + 4);
+	transform_Img_single(f_data_double, rotated_img_0, f_dim[0], f_dim[1], 0,  0,  90,  f_dim[0]/2 + 4);
 
 	std::cout << "Displaying first image after transformation\n";
 	cv::Mat img_0_rot(cv::Size(f_dim[0],f_dim[1]),CV_64F,rotated_img_0);
 	cv::imshow("image_0_rot", img_0_rot);
-	cv::waitKey(10000);
+	cv::waitKey(2000);
+
+	std::cout << "\nTesting function transform_Img_multiple\n";
+	std::cout << "Displaying a few images\n";
+	cv::Mat img_5(cv::Size(f_dim[0],f_dim[1]),CV_64F, &f_data_double[f_dim[0]*f_dim[1]*5]);
+	cv::Mat img_200(cv::Size(f_dim[0],f_dim[1]),CV_64F,&f_data_double[f_dim[0]*f_dim[1]*200]);
+	cv::Mat img_1000(cv::Size(f_dim[0],f_dim[1]),CV_64F,&f_data_double[f_dim[0]*f_dim[1]*1000]);
+	cv::Mat win_mat(cv::Size(f_dim[0]*3,f_dim[1]), CV_64F);
+	img_5.copyTo(win_mat(cv::Rect(  0, 0, f_dim[0], f_dim[1])));
+	img_200.copyTo(win_mat(cv::Rect( f_dim[0], 0, f_dim[0], f_dim[1])));
+	img_1000.copyTo(win_mat(cv::Rect( f_dim[0]*2, 0, f_dim[0], f_dim[1])));
+
+    cv::namedWindow("Images");
+	cv::imshow("Images", win_mat);
+	cv::waitKey(3000);
+
+	std::cout << "Displaying a few images after transformation\n";
+
+	double * imgs_rotated = new double[f_dim[0]*f_dim[1]*f_dim[2]]();
+	transform_Img_multiple(f_data_double, imgs_rotated, f_dim[0], f_dim[1], f_dim[2], 0,  0,  90,  f_dim[0]/2 + 4);
+	cv::Mat img_5_rot(cv::Size(f_dim[0],f_dim[1]),CV_64F, &imgs_rotated[f_dim[0]*f_dim[1]*5]);
+	cv::Mat img_200_rot(cv::Size(f_dim[0],f_dim[1]),CV_64F,&imgs_rotated[f_dim[0]*f_dim[1]*200]);
+	cv::Mat img_1000_rot(cv::Size(f_dim[0],f_dim[1]),CV_64F,&imgs_rotated[f_dim[0]*f_dim[1]*1000]);
+	cv::Mat win_mat_rot(cv::Size(f_dim[0]*3,f_dim[1]), CV_64F);
+	img_5_rot.copyTo(win_mat_rot(cv::Rect(  0, 0, f_dim[0], f_dim[1])));
+	img_200_rot.copyTo(win_mat_rot(cv::Rect( f_dim[0], 0, f_dim[0], f_dim[1])));
+	img_1000_rot.copyTo(win_mat_rot(cv::Rect( f_dim[0]*2, 0, f_dim[0], f_dim[1])));
+
+	cv::namedWindow("Images_rotated");
+	cv::imshow("Images_rotated", win_mat_rot);
+	cv::waitKey(3000);
+
+
+
 
 
 
@@ -74,5 +108,6 @@ int main(void){
 
 	delete [] f_data_double; f_data_double = NULL;
 	delete [] rotated_img_0; rotated_img_0 = NULL;
+	delete [] imgs_rotated; imgs_rotated = NULL;
 
 }
